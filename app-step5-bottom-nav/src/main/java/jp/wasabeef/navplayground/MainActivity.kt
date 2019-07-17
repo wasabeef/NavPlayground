b3158139d6c5809badd271e9845377b2cb15726f
wasabeef <dadadada.chop@gmail.com>
2019-07-18 01:21:28 +0900

    refacor

--- app-step5-bottom-nav/src/main/java/jp/wasabeef/navplayground/MainActivity.kt
+++ app-step5-bottom-nav/src/main/java/jp/wasabeef/navplayground/MainActivity.kt
@@ -2,28 +2,28 @@ package jp.wasabeef.navplayground
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
-        val appBarConfiguration =
-            AppBarConfiguration(
+        setupActionBarWithNavController(
+            navController, AppBarConfiguration(
                 setOf(
                     R.id.fragment_home,
                     R.id.fragment_dashboard
                 )
             )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        )
+        nav_view.setupWithNavController(navController)
     }
 }
